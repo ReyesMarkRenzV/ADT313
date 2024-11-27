69cb3fbdab008904c31adb7e9e685a3e4e8e302e
Mark Renz Reyes <147915413+ReyesMarkRenzV@users.noreply.github.com>
2024-11-28 00:18:20 +0800

    Add files via upload

---
+++ ADT313-IT3B PHP/array.php
@@ -0,0 +1,34 @@
+<h1>array<h1>
+
+<?php
+    $cars = array("volvo", "BMW", "Toyota");
+
+    $cars = ["volvo", "BMW", "Toyota"];
+
+
+    echo $cars[1];
+
+    $userInformation = array (
+        "firstName" =>"lem",
+        "lastName" =>"Francisco",
+        "role" => "admin"
+
+    );
+
+    echo $userInformation ['firstName'];
+
+
+    print_r($userInformation);
+
+    echo "<br/>";
+
+    var_dump($userInformation);
+
+
+
+
+    $user = array(
+        "information"
+    )
+
+?>
